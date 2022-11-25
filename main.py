c15906b19273b1fc3693a95290952b34390cbb2d
willy le roy <w.leroy@optline.fr>
2022-11-25 09:10:05 +0100

    Attempt to speed things up

--- main.py
+++ main.py
@@ -184,4 +184,4 @@ while not game.board.is_game_over() and not game.commentator.game_state.resign_o
         move_fgbg.apply(frame)
         previous_frame_queue.append(frame)
 cv2.destroyAllWindows()
-time.sleep(2)
+time.sleep(1)
